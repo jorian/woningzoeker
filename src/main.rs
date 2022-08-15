266f7cdfe3c245ef76f96b31b8ed68faf3a34f62
jorian <10341677+jorian@users.noreply.github.com>
2022-08-15 10:39:25 +0200

    overhaul

--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,160 @@
-use std::{collections::HashMap, thread, time::Duration};
+use std::{thread, time::Duration};
 
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, Url};
 use serde_json::{json, Value};
 
 fn main() {
-    let j = thread::spawn(|| {
-        vesteda();
+    let mut vesteda = Makelaar::Vesteda(Agent {
+        base_url: Url::parse("https://www.vesteda.com/api/units/search").unwrap(),
+        client: reqwest::blocking::Client::new(),
+        houses: vec![],
     });
 
-    let k = thread::spawn(|| {
-        rebo();
+    let mut rebo = Makelaar::Rebo(Agent {
+        base_url: Url::parse("https://www.rebohuurwoning.nl/object/search/list").unwrap(),
+        client: reqwest::blocking::Client::new(),
+        houses: vec![],
     });
 
-    let v = vec![j, k];
+    let mut handles = vec![];
 
-    for handle in v {
-        handle.join();
+    handles.push(thread::spawn(move || {
+        vesteda.query();
+        thread::sleep(Duration::from_secs(180));
+    }));
+
+    handles.push(thread::spawn(move || {
+        rebo.query();
+        thread::sleep(Duration::from_secs(180));
+    }));
+
+    for handle in handles {
+        let _ = handle.join();
     }
 }
 
-fn vesteda() {
-    let mut house_keeper = HouseKeeper {
-        n_houses: 0,
-        list: vec![],
-    };
+pub struct Agent {
+    base_url: Url,
+    client: reqwest::blocking::Client,
+    houses: Vec<Value>,
+}
 
-    let client = reqwest::blocking::Client::new();
+pub enum Makelaar {
+    Vesteda(Agent),
+    Rebo(Agent),
+}
 
-    let json = json!({
-        "place": "Zutphen, Nederland",
-	"placeObject": {
-		"name": "Zutphen, Nederland",
-		"placeType": "1",
-		"lng": "6.19605827",
-		"lat": "52.1427345"
-	},
-	"placeType": 1,
-	"priceFrom": 500,
-	"priceTo": 9999,
-	"sizes": [],
-	"sortType": 0,
-	"bedRooms": 0,
-	"rootId": 1303,
-	"unitTypes": [
-		2,
-		1,
-		4
-	],
-	"other": [],
-	"lat": "52.1427345",
-	"lng": "6.19605827",
-	"radius": 10});
+trait Queryable {
+    fn query(&mut self);
+}
 
-    loop {
-        println!("pinging vesteda");
-        let res = client
-            .post("https://www.vesteda.com/api/units/search")
-            .json(&json)
-            .send()
-            .unwrap()
-            .text()
-            .unwrap();
+impl Queryable for Makelaar {
+    fn query(&mut self) {
+        match self {
+            Makelaar::Rebo(agent) => {
+                let res = agent
+                    .client
+                    .post(agent.base_url.as_ref())
+                    .send()
+                    .unwrap()
+                    .text()
+                    .unwrap();
 
-        let v: Value = serde_json::from_str(&res).unwrap();
-        let v_items = v["items"].clone();
+                let v: Value = serde_json::from_str(&res).unwrap();
+                let v_items = v["data"]["items"].clone();
 
-        let v_arr = v_items.to_owned();
-        let found = v_arr.as_array().cloned().unwrap();
+                let v_arr = v_items.to_owned();
+                let v_arr = v_arr.as_array().cloned().unwrap();
 
-        for item in &found {
-            if house_keeper.list.iter().any(|hk| hk["id"] == item["id"]) {
-                println!("house already known");
-            } else {
-                println!("house not known! notify!");
-                send_telegram(
-                    (
-                        item["street"].as_str().unwrap(),
-                        item["houseNumber"].as_str().unwrap_or("0"),
-                        item["url"].as_str().unwrap(),
-                    ),
-                    &client,
-                );
+                let found: Vec<_> = v_arr
+                    .into_iter()
+                    .filter(|h| h["city"] == "Zutphen")
+                    .collect();
+
+                for item in &found {
+                    if agent.houses.iter().any(|hk| hk["id"] == item["id"]) {
+                        println!("house already known");
+                    } else {
+                        println!("house not known! notify!");
+                        send_telegram(
+                            (
+                                item["street"].as_str().unwrap(),
+                                item["street_number"].as_str().unwrap(),
+                                &format!(
+                                    "{}{}",
+                                    "https://rebohuurwoning.nl",
+                                    item["object_url"].as_str().unwrap()
+                                ),
+                            ),
+                            &agent.client,
+                        );
+                    }
+                }
+
+                agent.houses = found;
+            }
+            Makelaar::Vesteda(agent) => {
+                let json = json!({
+                    "place": "Zutphen, Nederland",
+                    "placeObject": {
+                        "name": "Zutphen, Nederland",
+                        "placeType": "1",
+                        "lng": "6.19605827",
+                        "lat": "52.1427345"
+                    },
+                    "placeType": 1,
+                    "priceFrom": 500,
+                    "priceTo": 9999,
+                    "sizes": [],
+                    "sortType": 0,
+                    "bedRooms": 0,
+                    "rootId": 1303,
+                    "unitTypes": [
+                        2,
+                        1,
+                        4
+                    ],
+                    "other": [],
+                    "lat": "52.1427345",
+                    "lng": "6.19605827",
+                    "radius": 10
+                });
+
+                let res = agent
+                    .client
+                    .post(agent.base_url.as_ref())
+                    .json(&json)
+                    .send()
+                    .unwrap()
+                    .text()
+                    .unwrap();
+
+                let v: Value = serde_json::from_str(&res).unwrap();
+                let v_items = v["items"].clone();
+
+                let v_arr = v_items.to_owned();
+                let found = v_arr.as_array().cloned().unwrap();
+
+                for item in &found {
+                    if agent.houses.iter().any(|hk| hk["id"] == item["id"]) {
+                        println!("house already known");
+                    } else {
+                        println!("house not known! notify!");
+                        send_telegram(
+                            (
+                                item["street"].as_str().unwrap(),
+                                item["houseNumber"].as_str().unwrap_or("0"),
+                                item["url"].as_str().unwrap(),
+                            ),
+                            &agent.client,
+                        );
+                    }
+                }
+
+                agent.houses = found;
             }
         }
-
-        house_keeper.list = found;
-
-        thread::sleep(Duration::from_secs(150));
     }
-}
-
-fn rebo() {
-    let mut house_keeper = HouseKeeper {
-        n_houses: 0,
-        list: vec![],
-    };
-
-    let client = reqwest::blocking::Client::new();
-
-    let mut headers = HashMap::new();
-    headers.insert("location", "Zutphen");
-
-    loop {
-        println!("pinging rebo");
-
-        let res = client
-            .post("https://www.rebohuurwoning.nl/object/search/list")
-            .form(&headers)
-            .send()
-            .unwrap()
-            .text()
-            .unwrap();
-
-        let v: Value = serde_json::from_str(&res).unwrap();
-        let v_items = v["data"]["items"].clone();
-
-        let v_arr = v_items.to_owned();
-        let v_arr = v_arr.as_array().cloned().unwrap();
-
-        let found: Vec<_> = v_arr
-            .into_iter()
-            .filter(|h| h["city"] == "Zutphen")
-            .collect();
-
-        for item in &found {
-            if house_keeper.list.iter().any(|hk| hk["id"] == item["id"]) {
-                println!("house already known");
-            } else {
-                println!("house not known! notify!");
-                send_telegram(
-                    (
-                        item["street"].as_str().unwrap(),
-                        item["street_number"].as_str().unwrap(),
-                        &format!(
-                            "{}{}",
-                            "https://rebohuurwoning.nl",
-                            item["object_url"].as_str().unwrap()
-                        ),
-                    ),
-                    &client,
-                );
-            }
-        }
-
-        house_keeper.list = found;
-
-        thread::sleep(Duration::from_secs(150));
-    }
-}
-
-pub struct HouseKeeper {
-    pub n_houses: usize,
-    pub list: Vec<Value>,
 }
 
 pub fn send_telegram(house: (&str, &str, &str), client: &Client) {
